c11cb6bd225fee168a7fae184a1964a41541534e
Beknight <benjamin.fan11.11@gmail.com>
2014-07-02 08:16:52 +1000

    added the package for the stat screen created the layout & class for stat screen (incomplete)

---
+++ src/com/pangers/StatScreen/StatScreen.java
@@ -0,0 +1,23 @@
+package com.pangers.StatScreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.pangers.tinyTony.R;
+
+public class StatScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.stats);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+
+}
