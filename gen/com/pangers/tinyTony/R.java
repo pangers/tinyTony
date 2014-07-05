fcef87090368a51a4eb41d28f00ca953ff0ea0da
pangers <j.pang91@gmail.com>
2014-07-05 14:22:14 +1000

    nav drawer added, but few bugs

--- gen/com/pangers/tinyTony/R.java
+++ gen/com/pangers/tinyTony/R.java
@@ -8,37 +8,46 @@
 package com.pangers.tinyTony;
 
 public final class R {
+    public static final class array {
+        public static final int drawermenu=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class color {
         public static final int blue=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_menu_btn_add=0x7f020001;
-        public static final int ic_menu_emoticons=0x7f020002;
-        public static final int ic_menu_settings_holo_light=0x7f020003;
-        public static final int whitebackground=0x7f020004;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_menu_btn_add=0x7f020003;
+        public static final int ic_menu_emoticons=0x7f020004;
+        public static final int ic_menu_settings_holo_light=0x7f020005;
+        public static final int whitebackground=0x7f020006;
     }
     public static final class id {
-        public static final int about=0x7f080008;
-        public static final int add=0x7f080006;
-        public static final int newtask=0x7f080000;
-        public static final int settings=0x7f080007;
-        public static final int tasklist=0x7f080003;
-        public static final int taskname=0x7f080004;
-        public static final int testtitle=0x7f080002;
-        public static final int timeleft=0x7f080005;
-        public static final int timeremaining=0x7f080001;
+        public static final int about=0x7f09000b;
+        public static final int add=0x7f090009;
+        public static final int contentframe=0x7f090001;
+        public static final int drawerlayout=0x7f090000;
+        public static final int drawerlist=0x7f090002;
+        public static final int newtask=0x7f090003;
+        public static final int settings=0x7f09000a;
+        public static final int tasklist=0x7f090006;
+        public static final int taskname=0x7f090007;
+        public static final int testtitle=0x7f090005;
+        public static final int timeleft=0x7f090008;
+        public static final int timeremaining=0x7f090004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int newtaskdialog=0x7f030001;
-        public static final int taskfragment=0x7f030002;
-        public static final int tasklistitem=0x7f030003;
+        public static final int drawer_row=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int newtaskdialog=0x7f030002;
+        public static final int taskfragment=0x7f030003;
+        public static final int tasklistitem=0x7f030004;
     }
     public static final class menu {
-        public static final int actions=0x7f070000;
+        public static final int actions=0x7f080000;
     }
     public static final class string {
         public static final int about=0x7f050002;
@@ -46,6 +55,8 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int cancel=0x7f05000a;
         public static final int dialogtitle=0x7f050009;
+        public static final int drawerclose=0x7f05000d;
+        public static final int draweropen=0x7f05000c;
         public static final int hello_world=0x7f05000b;
         public static final int newtask=0x7f050007;
         public static final int settings=0x7f050003;
@@ -65,10 +76,10 @@ public final class R {
             backward-compatibility can go here.
 		
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
